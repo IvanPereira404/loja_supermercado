1bf06fc2d16591376e57c0f28f0fbe30b63b7aeb
Xavier2244 <a89765@ualg.pt>
2025-12-16 14:02:46 +0000

    Criar produto

--- produtos.py
+++ produtos.py
@@ -12,3 +12,24 @@ def salvar_dados(dados):
     with open(FICHEIRO, "w", encoding="utf-8") as f:
         json.dump(dados, f, indent=4, ensure_ascii=False)
 
+def criar_produto(nome, categoria, preco_custo, preco_venda, estoque, codigo_barras, validade):
+    dados = carregar_dados()
+
+    ultimo_id = max([int(p["id"]) for p in dados], default=0)
+    novo_id = ultimo_id + 1
+
+    produto = {
+        "id": str(novo_id),
+        "nome": nome,
+        "categoria": categoria,
+        "preco_custo": str(preco_custo),
+        "preco_venda": str(preco_venda),
+        "estoque": str(estoque),
+        "codigo_barras": codigo_barras,
+        "validade": validade
+    }
+
+    dados.append(produto)
+    salvar_dados(dados)
+
+    return produto
