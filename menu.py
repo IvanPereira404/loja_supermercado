93f78ea309aa59626c74dc7feb9d23fb7451754f
diogo-131 <a89756@ualg.pt>
2025-12-21 22:18:51 +0000

    implementacao no menu as funicionalidades de manipulacao dos clientes com opções para criar, listar e atualizar clientes

--- menu.py
+++ menu.py
@@ -1,4 +1,5 @@
 from produtos import *
+from crudclientes import *
 
 def menu_principal():
     print("Menu:")
@@ -48,6 +49,40 @@ def menu_opc_4():
         elif escolha == '5':
             apagar_produto()
         elif escolha == '6':
+            print("Saindo...")
+            menu_principal()
+        else:
+            print("Opção inválida! Tente novamente.")
+
+
+def menu_opc_1():
+    print("Menu Clientes:")
+    print("1. Criar/Adicionar Cliente")
+    print("2. Listar Clientes")
+    print("3. Atualizar Cliente")
+    print("4. Sair")
+
+    while True:
+        escolha = input("Escolha uma opção(1-4): ")
+        
+        if escolha == '1':
+            nome = input("Nome: ")
+            telefone = input("Telefone: ")
+            email = input("Email: ")
+            endereco = input("Endereço: ")
+            data_cadastro = input("Data de Cadastro (YYYY-MM-DD): ")
+            create_cliente(nome, telefone, email, endereco, data_cadastro)
+        elif escolha == '2':
+            read_clientes()
+        elif escolha == '3':
+            id_cliente = int(input("ID do Cliente a atualizar: "))
+            nome = input("Novo Nome (deixe em branco para não alterar): ") or None
+            telefone = input("Novo Telefone (deixe em branco para não alterar): ") or None
+            email = input("Novo Email (deixe em branco para não alterar): ") or None
+            endereco = input("Novo Endereço (deixe em branco para não alterar): ") or None
+            data_cadastro = input("Nova Data de Cadastro (YYYY-MM-DD) (deixe em branco para não alterar): ") or None
+            update_cliente(id_cliente, nome, telefone, email, endereco, data_cadastro)
+        elif escolha == '4':
             print("Saindo...")
             menu_principal()
         else:
